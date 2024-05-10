e9e2bc7f411f0a48a59f5cf81bf9032a2f671432
yusinh <yysh1020@gmail.com>
2024-05-10 17:31:23 +0900

    로그인기 기능구현1_로그인과 회원과입 url만 허용하고 그외 요청 막기

--- src/main/java/com/example/ms1/note/member/MemberController.java
+++ src/main/java/com/example/ms1/note/member/MemberController.java
@@ -46,4 +46,9 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
 }
---
+++ src/main/java/com/example/ms1/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.ms1.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login", "/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin.loginPage("/login")
+                );
+        return http.build();
+    }
+}
